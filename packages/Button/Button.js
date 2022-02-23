f3c0a9ed564c60b0e94285dd335f8e0753d06889
mattcbodily <mattcbodily@gmail.com>
2022-02-23 11:19:50 -0700

    Added a couple props

--- packages/Button/Button.js
+++ packages/Button/Button.js
@@ -1,7 +1,10 @@
 import React from 'react'
 
 export const Button = React.forwardRef((props, ref) => (
-  <button ref={ref}>
+  <button 
+    onClick={props.onClick}
+    ref={ref}
+  >
     {props.children}
   </button>
 ))
--- packages/Input/Input.js
+++ packages/Input/Input.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
 export const Input = React.forwardRef((props, ref) => (
-  <input 
+  <input
     onChange={props.onChange}
     ref={ref}
+    type={props.inputTypes || 'text'}
     value={props.value}
   />
 ))
